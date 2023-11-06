9dce8a33f4551a1aef26d000008db7a394ee7d1b
mohammadjavvadmohammadi <149230041+mohammadjavvadmohammadi@users.noreply.github.com>
2023-11-05 23:41:49 -0800

    Add files via upload

---
+++ game1.py
@@ -0,0 +1,79 @@
+#rock paper scissor game
+
+rock="sang"
+paper= "kaghaz"
+scissor= "gheychi"
+p1= input("please start player1 : ")
+p2= input("please start player2 : ")
+if p1 == rock and p2 == scissor :
+   print("player1 win")
+elif p1 == scissor and p2 == rock :
+     print("pleyer2 win")
+elif p1 == rock and p2 == rock :
+     print("equal result")
+elif p1 == scissor and p2 == scissor :
+     print("equal result")
+elif p1 == rock and p2 == paper :
+     print("player2 win")
+elif p1 == paper and p2 == rock :
+     print("player1 win")
+elif p1 == paper and p2 == paper :
+     print("equal result")
+elif p1 == paper and p2 == scissor :
+     print("player2 win")
+elif p1 == scissor and p2 == paper :
+     print("player1 win")
+else :
+     print("error")
+
+
+p1= input("please start player1 : ")
+p2= input("please start player2 : ")
+if p1 == rock and p2 == scissor :
+   print("player1 win")
+elif p1 == scissor and p2 == rock :
+     print("pleyer2 win")
+elif p1 == rock and p2 == rock :
+     print("equal result")
+elif p1 == scissor and p2 == scissor :
+     print("equal result")
+elif p1 == rock and p2 == paper :
+     print("player2 win")
+elif p1 == paper and p2 == rock :
+     print("player1 win")
+elif p1 == paper and p2 == paper :
+     print("equal result")
+elif p1 == paper and p2 == scissor :
+     print("player2 win")
+elif p1 == scissor and p2 == paper :
+     print("player1 win")
+else :
+     print("error")
+
+p1= input("please start player1 : ")
+p2= input("please start player2 : ")
+if p1 == rock and p2 == scissor :
+   print("player1 win")
+elif p1 == scissor and p2 == rock :
+     print("pleyer2 win")
+elif p1 == rock and p2 == rock :
+     print("equal result")
+elif p1 == scissor and p2 == scissor :
+     print("equal result")
+elif p1 == rock and p2 == paper :
+     print("player2 win")
+elif p1 == paper and p2 == rock :
+     print("player1 win")
+elif p1 == paper and p2 == paper :
+     print("equal result")
+elif p1 == paper and p2 == scissor :
+     print("player2 win")
+elif p1 == scissor and p2 == paper :
+     print("player1 win")
+else :
+     print("error")
+
+
+
+
+
