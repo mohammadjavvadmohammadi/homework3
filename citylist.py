07dbc52e3b311f57d49b1f875f019a6a1de4e0e5
mohammadjavvadmohammadi <149230041+mohammadjavvadmohammadi@users.noreply.github.com>
2023-11-20 00:52:18 +0330

    Update citylist.py

--- citylist.py
+++ citylist.py
@@ -1,19 +1,23 @@
 # city list
-city_list = ["sari" , "nowshahr" , "noor" , "chalous" , "ramsar" , "namakabrood"]
-print(city_list[+0])
-print(city_list[+5])
-print(city_list[+4])
-print(city_list[-3])
+city_list = ["sari" , "nowshahr" , "noor" , "chalous" ,
+              "ramsar" , "namakabrood"]
+print(city_list[-2])
+print(city_list[-4])
 print(city_list[-1])
-print(city_list[-0])
-city_list.extend("nowshahr")
+print(city_list[0])
+print(city_list[1])
+print(city_list[2])
+city_list.append("noor")
 print(city_list)
-city_list.remove("sari")
+city_list.extend("noor")
 print(city_list)
-city_list.append("lahijan")
+city_list.insert(-2 , "noor")
 print(city_list)
-city_list.insert(3 , "nowshahr")
+city_list.pop(4)
 print(city_list)
-city_list.pop(1)
+city_list.remove("noor")
 print(city_list)
-print(len(city_list))
+
+
+
+
